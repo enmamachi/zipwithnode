72cf8d8e99010980ce392708bbea014c2efaad15
enmamachi <141737763+enmamachi@users.noreply.github.com>
2025-04-08 11:27:26 +0700

    Update zip.js

--- zip.js
+++ zip.js
@@ -1,23 +1,14 @@
 const inquirer = require('inquirer').default;
 const fs = require('fs');
 const path = require('path');
-const { ZipFile } = require('zip-lib');
+const zip = require('zip-a-folder');
 
-async function compressFolder(folderPath, zipFilePath, password) {
+async function compressFolder(folderPath, zipFilePath) {
   try {
-    // Buat instance ZipFile
-    const zip = new ZipFile();
+    // Kompres folder
+    await zip.zip(folderPath, zipFilePath);
 
-    // Tambahkan folder ke dalam zip
-    await zip.addFolder(folderPath);
-
-    // Set password untuk zip
-    zip.setPassword(password);
-
-    // Simpan zip ke file
-    await zip.saveAs(zipFilePath);
-
-    console.log(`Folder ${folderPath} telah dikompresi menjadi ${zipFilePath} dengan password: ${password}`);
+    console.log(`Folder ${folderPath} telah dikompresi menjadi ${zipFilePath}`);
   } catch (err) {
     console.error('Terjadi kesalahan saat mengompres folder:', err);
   }
@@ -33,11 +24,10 @@ inquirer.prompt([
 ]).then(answers => {
   const folderPath = answers.folderPath;
   const zipFilePath = path.join(folderPath, 'output.zip');
-  const password = 'abcd';
 
   // Pastikan folder yang akan dikompresi ada
   if (fs.existsSync(folderPath)) {
-    compressFolder(folderPath, zipFilePath, password);
+    compressFolder(folderPath, zipFilePath);
   } else {
     console.error(`Folder tidak ditemukan: ${folderPath}`);
   }
